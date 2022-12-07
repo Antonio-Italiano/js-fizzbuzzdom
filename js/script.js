6ccc083eb90304986007a8c3326f94e74d4dcfea
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-07 18:38:28 +0100

    Add innerHTML

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
+const main = document.getElementById('main')
+
 //1# Scrivo un programma che stampi in console i numeri da 1 a 100
+
+let content ='<div class="row justify-content-center">';
+
 for (let i = 1; i <= 100; i++) {
    
     
+
+
     if (i%15 == '') {
         let i = 'FizzBuzz';
         console.log(i);
+        const number = i ;
+        if (number){
+        content += `<div class="col-2 m-2 d-flex justify-content-center align-items-center bg-danger">${number}</div>`
+        }
     } else if (i%5 == '') {
         let i = 'Buzz';
-        console.log(i);        
+        console.log(i);
+        const number = i ;
+        if (number){
+        content += `<div class="col-2 m-2 d-flex justify-content-center align-items-center bg-warning">${number}</div>`
+        }        
     } else if (i%3 == '') {
         let i = 'Fizz';
         console.log(i);
+        const number = i ;
+        if (number){
+        content += `<div class="col-2 m-2 d-flex justify-content-center align-items-center bg-success">${number}</div>`
+        }
     } else {
         console.log(i);
+        const number = i ;
+        if (number){
+        content += `<div class="col-2 m-2 d-flex justify-content-center align-items-center bg-primary">${number}</div>`
+        }
     }    
     
-} 
 
+
+    
+
+} 
+content += '</div>';
+main.innerHTML = content;
