2fa885fc2b955173e26b14826c3c9ac232d737f6
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-07 17:09:03 +0100

    Add multipli di 15 "FlizzBuzz"

--- js/script.js
+++ js/script.js
@@ -2,20 +2,18 @@
 for (let i = 1; i <= 100; i++) {
    
     
-    if (i%3 == '') {
-        let i = 'Fizz';
+    if (i%15 == '') {
+        let i = 'FizzBuzz';
         console.log(i);
     } else if (i%5 == '') {
         let i = 'Buzz';
         console.log(i);        
-    } else if (i%15 == '') {
-        let i = 'FizzBuzz';
+    } else if (i%3 == '') {
+        let i = 'Fizz';
         console.log(i);
     } else {
         console.log(i);
-    }
-    
-    
+    }    
     
 } 
 
