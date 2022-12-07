f9877c31cca9c9d8610bf67a60e876ee73ea3e1f
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-07 16:36:30 +0100

    Add multipli di 5 “Buzz”

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 //1# Scrivo un programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+   
     
     if (i%3 == '') {
-        let i = 'fizz';
+        let i = 'Fizz';
         console.log(i);
-    }    
-    // } else if (i%5) {
-    //     let i = 'Buzz';
-    //     console.log(i);
-    // 
+    } else if (i%5 == '') {
+        let i = 'Buzz';
+        console.log(i);        
+    } else {
+        console.log(i);
+    }
+    
+    
     
 } 
 
